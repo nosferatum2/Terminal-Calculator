b8deac8e09d55a9a8a84e3e48144d38757314eb0
nosferatum2 <nosferatum3@i.ua>
2020-04-01 18:53:17 +0300

    Added check value. Format errors fixed. Dont fixed error with transmition empty user input.

--- Calc.py
+++ Calc.py
@@ -2,27 +2,50 @@ import re
 
 print("Welcome to my calculator")
 
-def get_input():
-    a = str(input("Enter some number: ")).replace(",",".")
-    return a
 
-def test_input():
+def get_value():
+    """
+    User input value.
+    """
+    val = str(input(f"{msg}")).replace(",", ".")
+    return val
+
+
+def test_value():
+    """Function checks user input for a decimal numbers and mathematical actions.
+    Added check for exclude 'leading zero'.
+    """
     while True:
-        a = get_input()
-        # Регулярка делится на 3 блока: (Целые числа |С плавающей точкой | Действия)
-        if not re.match(r"^[0-9]*$|^[0-9]*.[0-9]*$|^[\+\-\*\/]{1}$",a):
+        val = get_value()
+        # Регулярка делится на: (Целые числа |С плавающей точкой | Отрицательные целые числа |Отрицательные с плавающей точкой)
+        r_nums = re.match(r"^[0-9]*$|^[0-9]*\.[0-9]*$|^\-[0-9]*$|^\-[0-9]*\.[0-9]*$", val)
+        # Регулярка действия
+        r_action = re.match(r"^[\+\-\*\/]{1}$", val)
+
+        if not ((r_nums and i % 2 == 0) or (r_action and i % 2 == 1)):
             print("Incorrect value! repeat Entry!")
         else:
-            return a
-
+            while val[0] == "0" and len(val) > 1:
+                val = val[1:]
+            else:
+                return val
 
 
 def calc():
-    txt = ""
+    """Function for adding a String value from input.
+    """
+    str1 = ""
     s = 3
+    global msg, i  # msg Global declaration for user input
+    msg = ""
     for i in range(s):
-        txt += test_input()
-    return txt
+        if i % 2 != 0:
+            msg = "Enter an action: "
+        else:
+            msg = "Enter some number: "
+        # writes iterable value in string
+        str1 += test_value()
+    return str1
 
 
 res = eval(calc())
